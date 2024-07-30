d6b6196fbb3392deb7fbc6d813d8200378ee7d03
TamjidBhai <tamtop2000@gmail.com>
2024-07-30 22:26:40 +0600

    Added  single page(still tinkering)

---
+++ vite-project/src/components/slider/slider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const slider = () => {
+  return (
+    <div className='slider'>
+      slider
+    </div>
+  )
+}
+
+export default slider
--- vite-project/src/routes/singlePage/singlePage.jsx
+++ vite-project/src/routes/singlePage/singlePage.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import './singlePage.css'
+import React from "react";
+import "./singlePage.css";
+import Slider from "../../components/slider/slider";
+import { singlePostData } from "../../lib/dummyData";
+import { userData } from "../../lib/dummyData";
 
 const singlePage = () => {
   return (
-    <div>singlePage</div>
-  )
-}
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h2>{singlePostData.title}</h2>
 
-export default singlePage
+                <div className="address">
+                  <img src="../images/pin.png" alt="" />
+                  <p id="address">{singlePostData.address}</p>
+                </div>
+
+                <span id="price">$ {singlePostData.price}</span>
+              </div>
+
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <p>{userData.name}</p>
+              </div>
+            </div>
+
+            <div className="bottom">{singlePostData.desc}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="features">
+        <div className="wrapper">
+          <h1>features</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default singlePage;
