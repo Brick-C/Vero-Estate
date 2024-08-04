01f90b75c84706afd0d2b70a56970bf7a10cdfd4
TamjidBhai <tamtop2000@gmail.com>
2024-08-04 20:22:41 +0600

    Added chat box -problem with chat box

--- vite-project/src/components/chat/chat.jsx
+++ vite-project/src/components/chat/chat.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chat.css";
 
 const chat = () => {
+  const [chat, setChat] = useState(true);
+
   return (
     <div className="chat">
       <div className="messages">
@@ -52,7 +54,47 @@ const chat = () => {
         </div>
       </div>
 
-      <div className="chatBox">Chat</div>
+      {chat && (
+        <div className="chatBox">
+          <div className="top">
+            <div className="user">
+              <img
+                src="https://images.unsplash.com/photo-1640951613773-54706e06851d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHVzZXJ8ZW58MHx8MHx8fDA%3D"
+                alt=""
+              />
+              John Doe
+            </div>
+            <span onClick={() => setChat(null)} className="close">
+              X
+            </span>
+          </div>
+          <div className="center">
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessage">
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+              <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessage own">
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+              <span>1 hour ago</span>
+            </div>
+          </div>
+          <div className="bottom">
+            <textarea></textarea>
+            <button>Send</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
